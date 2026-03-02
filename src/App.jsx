9bc6e5c937ce658cc2d8d3d3ae6fa10a3e369626
andre-alcantara <andrealcantara.co@gmail.com>
2026-03-02 17:05:28 -0300

    feat: add external links in hero text

--- src/App.jsx
+++ src/App.jsx
@@ -18,28 +18,28 @@ export default function App() {
 
         <main className="col col-right">
           <div className="top">
-            <span>Design @ Mercado Livre</span>
-            <span>Linkedin</span>
+            <a>Design @ Mercado Livre</a>
+            <a className="muted" href="https://www.linkedin.com/in/and-alcantara/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
           </div>
           <article className="hero">
             <p>
-              <span>I'm André Alcantara, a brazilian designer based in São Paulo, over the past 5 years, I've had the privilege of working with brands like </span>
-              <span className="muted">Itaú</span>
-              <span>, </span>
-              <span className="muted">Raízen</span>
-              <span> and </span>
-              <span className="muted">MRV</span>
-              <span>, bringing clarity to complex problems with intentional decisions and attention to </span>
-              <span className="muted">craft</span>.<br />
-              <span>Currently at </span>
-              <span className="muted">Mercado Livre</span>
-              <span>.</span>
+              <a>I'm André Alcantara, a brazilian designer based in São Paulo, over the past 5 years, I've had the privilege of working with brands like </a>
+              <a className="muted" href="https://www.itau.com.br" target="_blank" rel="noopener noreferrer">Itaú</a>
+              <a>, </a>
+              <a className="muted" href="https://www.raizen.com.br/" target="_blank" rel="noopener noreferrer">Raízen</a>
+              <a> and </a>
+              <a className="muted" href="https://www.mrv.com.br/" target="_blank" rel="noopener noreferrer">MRV</a>
+              <a>, bringing clarity to complex problems with intentional decisions and attention to </a>
+              <a className="muted" href="https://www.doc.cc/syntax/craft" target="_blank" rel="noopener noreferrer">craft</a>.<br />
+              <a> Currently at </a>
+              <a className="muted" href="https://www.mercadolivre.com.br/" target="_blank" rel="noopener noreferrer">Mercado Livre</a>
+              <a>.</a>
             </p>
           </article>
           <div className="hour">
-            <span>São Paulo</span>
-            <span>–</span>
-            <span>{time}</span>
+            <a>São Paulo</a>
+            <a>–</a>
+            <a>{time}</a>
           </div>
         </main>
       </div>
